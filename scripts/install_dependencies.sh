3e9a6bbab5b0d81fca3cafd62e933152cb38f7e8
Geonhee Kim <43808162+kimgeonhee317@users.noreply.github.com>
2025-06-21 15:55:55 +0900

    set env

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,10 @@
-python -m pip install torch==2.2.0
-python -m pip install plotly==5.19.0
-python -m pip install numpy==1.26.4
-python -m pip install nbformat==5.9.2
-pytnon -m pip install kaleido==0.2.1
-python -m pip install circuitsvis==1.43.2
-python -m pip install scipy==1.13.1
+python -m pip install torch==2.6.0
+python -m pip install plotly==5.24.1
+python -m pip install numpy==2.0.2
+python -m pip install nbformat==5.10.4
+python -m pip install kaleido==0.2.1
+#python -m pip install circuitsvis==1.43.2
+python -m pip install scipy==1.15.3
 python -m pip install transformer_lens==2.11.0
 
 #python -m pip install -e.
